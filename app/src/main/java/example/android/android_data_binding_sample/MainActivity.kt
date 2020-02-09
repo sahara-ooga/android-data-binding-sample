3c2b5a9feac205ee5ac559d78797d624807e63b6
yuu ogasawara <performa532@gmail.com>
2020-02-09 19:56:37 +0900

    add binding property

--- app/src/main/java/example/android/android_data_binding_sample/MainActivity.kt
+++ app/src/main/java/example/android/android_data_binding_sample/MainActivity.kt
@@ -3,12 +3,13 @@ package example.android.android_data_binding_sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import example.android.android_data_binding_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
