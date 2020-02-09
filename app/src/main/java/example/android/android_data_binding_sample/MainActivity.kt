68b21bf03d5a25d6e26a5941993ccb7c58038f39
yuu ogasawara <performa532@gmail.com>
2020-02-09 19:45:51 +0900

    add view model property

--- app/src/main/java/example/android/android_data_binding_sample/MainActivity.kt
+++ app/src/main/java/example/android/android_data_binding_sample/MainActivity.kt
@@ -1,9 +1,14 @@
 package example.android.android_data_binding_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
